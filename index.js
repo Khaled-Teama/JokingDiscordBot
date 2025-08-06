ba09305b15937173788086a430d11d7c890b54e6
khaled hany <tetandro2@gmail.com>
2025-08-06 20:23:17 +0200

    addC

--- index.js
+++ index.js
@@ -7,12 +7,34 @@ const client = new Client ({intents : [GatewayIntentBits.Guilds,GatewayIntentBit
 const pingC = require(`./Commands/CommandsForHelp/ping`);
 
 
+
 client.once(Events.ClientReady,(client) => {
 
     console.log(`Hi Khaloody now i am ready for joking : ${client.user.tag}`)
 
 });
 
+client.on(Events.InteractionCreate,async (inter) =>{
+    if(!inter.isCommand()) {
+        return;
+    }
+    if(inter.commandName === pingC.data.name){
+            try {
+                await pingC.execute(inter);
+
+            } catch (error) {
+                    console.error(`error during execution`,error);
+                    await inter.reply({
+                        content : `فيه مشكلة`,
+                        ephemeral: true,
+                    });
+
+            }
+
+
+    }
+
+})
 
 
 
